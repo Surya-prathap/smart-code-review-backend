609d1fe0a2f54e8fb8ea39de5a0b7f079a72cb39
Surya-prathap <13794prathap@gmail.com>
2026-06-12 17:01:23 +0530

    Added structured error handling

--- src/main/java/com/smartreview/exception/GlobalExceptionHandler.java
+++ src/main/java/com/smartreview/exception/GlobalExceptionHandler.java
@@ -1,15 +1,19 @@
 package com.smartreview.exception;
 
+import com.smartreview.dto.ErrorResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ReviewNotFoundException.class)
-    public ResponseEntity<String> handleReviewNotFoundException(ReviewNotFoundException ex){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<ErrorResponseDTO> handleReviewNotFoundException(ReviewNotFoundException ex){
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(LocalDateTime.now(),HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseDTO);
     }
 }
